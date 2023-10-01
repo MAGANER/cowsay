bc160ba0efc5a6b1cf15dbcc1c23b7ca1b6d5a21
MAGANER <maganer.krik@mail.ru>
2023-10-01 12:53:09 +0300

    rewrite a bit

--- main.cpp
+++ main.cpp
@@ -9,26 +9,32 @@
 #include<unistd.h>
 #endif
 
-std::list<std::string> split_string_by_newline(const std::string& str)
+bool isnewline(int ch) 
 {
-	auto result = std::list<std::string>{};
-	auto ss = std::stringstream{ str };
-
-	for (std::string line; std::getline(ss, line, '\n');)
-		result.push_back(line);
-
-	return result;
-}
-size_t compute_horizontal_border_length(int argc, char** argv)
-{
-	auto max = argc < 10 ? argc : 10;
-
-	size_t s = 0;
-	for (size_t i = 0; i < max; i++)
-	{
-		s+=strlen(argv[i]); 
+	switch (ch) {
+	case '\r':
+	case '\n':
+	case '\f':
+		return true;
 	}
-	return s-1;
+	return false;
+}
+std::list<std::string> split_string(const std::string& str)
+{
+	std::list<std::string> strings;
+
+	std::string buffer;
+	for (auto& ch : str)
+	{
+		if (isnewline(ch))
+		{
+			strings.push_back(buffer);
+			buffer.clear();
+		}
+		else
+			buffer += ch;
+	}
+	return strings;
 }
 std::list<std::string> compute_lines(int argc, char** argv, size_t len)
 {
@@ -36,7 +42,7 @@ std::list<std::string> compute_lines(int argc, char** argv, size_t len)
 	std::list<std::string> lines;
 
 	std::string line;
-	for (size_t i = 2; i < argc; i++)
+	for (size_t i = 1; i < argc; i++)
 	{
 		if (line.size() + strlen(argv[i]) >= len)
 		{
@@ -54,22 +60,7 @@ std::list<std::string> compute_lines(int argc, char** argv, size_t len)
 	lines.push_back(line);
 	return lines;
 }
-std::list<std::string> read_lines(const std::string& file)
-{
-	//read file line by line, if it's only regular file
-
-	std::list<std::string> lines;
-	if (std::filesystem::is_regular_file(file))
-	{
-		std::ifstream input(file);
-		for (std::string line; getline(input, line); )
-		{
-			lines.push_back(line);
-		}
-	}
-	return lines;
-}
-inline void print_cow()
+void print_cow()
 {
 	//one system call is better then several
 	std::string cow;
@@ -83,53 +74,35 @@ inline void print_cow()
 	
 	printf(cow.c_str());
 }
-
-void print_message(size_t len,const std::list<std::string>& lines)
+void print_message(std::list<std::string>& lines)
 {
-	//print upper delimiter
-	for (size_t i = 0; i < len + 2; i++)printf("-");  //std::cout << "-";
-	printf("\n");
-
-	for (auto& l : lines)
+	auto max_size = (*std::max_element(lines.begin(), lines.end(),
+		[](auto& a, auto& b)
+		{
+			return a.size() < b.size();
+		}
+	)).size();
+	auto pad = [&](auto& s)
 	{
-		printf("| %s",l.c_str());
-		auto diff = abs((int)len - (int)l.size());
-		for (auto i = 0; i < diff; i++)printf(" "); //std::cout << " ";
+		s = "| " +s;
+		while (s.size() != max_size + 3)
+			s += " ";
+		s += "|";
+	};
 
-		printf("|\n");
-	}
-
-
-	//print bottom delimiter
-	for (size_t i = 0; i < len + 2; i++)printf("="); //std::cout << "=";
-
-	print_cow();
-}
-void print_empty_message()
-{
-	std::list<std::string> line;
-	line.push_back("moo?");
-	print_message(5, line);
-}
-size_t get_max_size(const std::list<std::string>& lines)
-{
-	//return the longest line in the list
-	size_t max = (*lines.begin()).size();
-	for (auto& l : lines)
+	printf("%s\n", std::string(max_size + 3, '-').c_str());
+	for (auto& line : lines)
 	{
-		if (l.size() > max)
-			max = l.size();
+		pad(line);
+		printf("%s\n", line.c_str());
 	}
-	return max;
-	
+	printf("%s\n", std::string(max_size + 3, '=').c_str());
 }
-
 int main(int argc, char** argv)
 {
 
 	char* src = nullptr;
 	size_t   len;
-
 #ifdef __linux__
   if(!isatty(0))
   {
@@ -137,10 +110,9 @@ int main(int argc, char** argv)
 	src = readall(stdin, &len);
 	if (src)
 	{
-		auto lines = split_string_by_newline(std::string(src));
-		auto len = get_max_size(lines);
-		print_message(len, lines);
-
+		auto lines = split_string(src);
+		print_message(lines);
+		print_cow();
 		free(src);
 	}
 #ifdef __linux__	
@@ -148,33 +120,20 @@ int main(int argc, char** argv)
 #endif
 	else
 	{
-		//read from cli arguments or from file
-		//if there is another argument, except the cowsay itself, then 
-		//we can check what is that option
-		auto spec = argc > 1 ? std::string(argv[1]) : "";
-
-
-		if (spec == "-t")
+		if (argc == 1)
 		{
-			auto len = compute_horizontal_border_length(argc, argv);
-			auto lines = compute_lines(argc, argv, len);
-			print_message(len, lines);
-		}
-		else if (spec == "-f" && argc == 3)
-		{
-			auto lines = read_lines(argv[2]);
-			if (lines.empty())
-				print_empty_message();
-			else
-			{
-				auto len = get_max_size(lines);
-				print_message(len, lines);
-			}
+			printf("Usage example #1: cat file | cowsay\n \
+				   Usage example #2: cowsay bla bla bla bla");
+			return 0;
 		}
 		else
 		{
-			print_empty_message();
+			//every line contains up to 20 characters
+			auto lines = compute_lines(argc, argv, 20);
+			print_message(lines);
+			print_cow();
 		}
+		
 	}
 
 	
